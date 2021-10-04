46da2a5e8816ee8b15a027de8a2d985ac73dccc8
YeongHyeon-Kim <insut69@gmail.com>
2021-10-05 00:45:47 +0900

    feat: mypage Route 추가

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Main from "./pages/Main";
 import Board from "./pages/Board";
 import Message from "./pages/Message";
 import DetailMessage from "./pages/MessageDetail";
-
+import Signup from "./pages/Account/Signup";
+import Login from "./pages/Account/Login";
+import MyPage from "./pages/Mypage/Main";
+import Auth from "./pages/Mypage/Auth/Main";
+import AuthDetail from "./pages/Mypage/Auth/Detail";
+import EditDetail from "./pages/Mypage/EditDetail";
 const App = () => {
     return (
         <BrowserRouter>
@@ -15,9 +20,12 @@ const App = () => {
                 <Route exact path="/board" component={Board} />
                 <Route exact path="/message" component={Message} />
                 <Route exact path="/message/:no" component={DetailMessage} />
-
+                <Route exact path="/mypage" component={MyPage} />
                 <Route exact path="/signup" component={Signup} />
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/mypage/auth" component={Auth} />
+                <Route exact path="/mypage/auth/detail" component={AuthDetail} />
+                <Route exact path="/mypage/edit" component={EditDetail} />
             </Switch>
         </BrowserRouter>
     );
